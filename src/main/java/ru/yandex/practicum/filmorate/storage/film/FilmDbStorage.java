255a4ec8bf02ccfb66750449a565cf184a1de0eb
Porf Paymulov <p.paymulov@gmail.com>
2024-01-01 23:40:33 +0300

    💡 Ветка для реализации задачи должна называться add-common-films.
    Описание задачи
    Реализовать вывод общих с другом фильмов с сортировкой по их популярности.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.DataNotFoundException;
